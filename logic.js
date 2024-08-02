9de1d157bfff1a2a18c7b682098cff260aed41ae
DarkStorm <150134437+DarkStormIX@users.noreply.github.com>
2024-08-02 19:43:48 +0800

    Add a getHumanChoice function
    the function ask player to choose an option

--- logic.js
+++ logic.js
@@ -1,15 +1,36 @@
+// create a function for computer to choose one option
 function getComputerChoice(){
-    let type = Math.floor(Math.random() * (Math.floor(3) - Math.ceil(1) + 1) + Math.ceil(1));
-    switch(type){
+    var computerChoice = Math.floor(Math.random() * (Math.floor(3) - Math.ceil(1) + 1) + Math.ceil(1));
+    switch(computerChoice){
         case 1:
-            return "Rock";
+            return "rock";
         case 2:
-            return "Paper";
+            return "paper";
         case 3:
-            return "Scissor";
+            return "scissors";
         default:
-            return "Error";
+            return "error";
     }
 }
 
-console.log("Computer choose " + getComputerChoice())
+console.log("Computer choose " + getComputerChoice());
+
+// create a function for player to choose one option
+function getHumanChoice(){
+    var humanChoice = prompt("Choose between rock, paper or scissors.");
+    var humanChoice = humanChoice.toLowerCase();
+
+    switch(humanChoice){
+        case "rock":
+            return humanChoice;
+        case "paper":
+            return humanChoice;
+        case "scissors":
+            return humanChoice;
+        default:
+            return "not available";
+    }
+}
+
+console.log("Player choose " + getHumanChoice());
+
