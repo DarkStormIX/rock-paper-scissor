2ef7133d7c71d698f011242a68cd719e146d33b5
DarkStorm <150134437+DarkStormIX@users.noreply.github.com>
2024-08-03 13:43:45 +0800

    Add a playRound function
    Let the game be played but only one round
    Compare human and computer choice and decide a winner

--- logic.js
+++ logic.js
@@ -13,7 +13,6 @@ function getComputerChoice(){
     }
 }
 
-console.log("Computer choose " + getComputerChoice());
 
 // create a function for player to choose one option
 function getHumanChoice(){
@@ -32,5 +31,42 @@ function getHumanChoice(){
     }
 }
 
-console.log("Player choose " + getHumanChoice());
+// create a logic to play a round
+// need to compare human and computer choice
+// need to give score when someone win a round
+var humanScore = 0;
+var computerScore = 0;
+
+function playRound(human, computer){
+    console.log("Player choose " + human);
+    console.log("Computer choose " + computer);
+
+    if(human == computer){
+        console.log("draw");
+        //score does not change
+    }
+    else if( (human == "rock" && computer == "scissors") 
+          || (human == "paper" && computer == "rock")
+          || (human == "scissors" && computer == "paper")){
+            // human win
+            // plusOne on human point
+            humanScore++;
+            console.log("Player win!");
+            console.log("Player score: " + humanScore);
+    }
+    else if( (computer == "rock" && human == "scissors") 
+          || (computer == "paper" && human == "rock")
+          || (computer == "scissors" && human == "paper")){
+            // computer win
+            // plusOne on computer point
+            computerScore++;
+            console.log("Computer win!");
+            console.log("Computer score: " + computerScore);
+    }
+    else{
+        console.log("logic error");
+    } 
+}
+
+playRound(getHumanChoice(),getComputerChoice());
 
